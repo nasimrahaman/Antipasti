8c2b2ddfcde3ccb27b4524d4e9513937d8cf7d8a
Nasim Rahaman <nasimrahaman@github.com>
2016-10-05 15:34:37 +0200

    - cleaned up wmap business

--- Projects/FIB25/Scripts/plot.py
+++ Projects/FIB25/Scripts/plot.py
@@ -25,7 +25,7 @@ def plot(net, trX, **plotconfig):
     # Loop over and evaluate
     for n, batch in enumerate(batches):
         # Read from batch
-        bX, bY, bW = batch
+        bX, bY = batch[0:2]
 
         print("[+] Evaluating batch {} of {}...".format(n, plotconfig['numbatches']))
         # Evaluate
@@ -34,10 +34,6 @@ def plot(net, trX, **plotconfig):
         vz.printensor2file(bX, savedir=plotconfig['plotdir'], mode='image', nameprefix='RAW{}--'.format(n))
         vz.printensor2file(bY, savedir=plotconfig['plotdir'], mode='image', nameprefix='GT{}--'.format(n))
         vz.printensor2file(ny, savedir=plotconfig['plotdir'], mode='image', nameprefix='PRED{}--'.format(n))
-        vz.printensor2file(bW, savedir=plotconfig['plotdir'], mode='image', nameprefix='WM{}--'.format(n))
-
-        print("[+] Weight map statisitics: min = {}, max = {}, mean = {}, std = {}".format(bW.min(), bW.max(),
-                                                                                           bW.mean(), bW.std()))
 
     # Done
     print("[+] Done.")
